070f69aaa26d3ee17ee38485e27490a68693933f
RaimisReej <54449879+RaimisReej@users.noreply.github.com>
2021-05-27 08:54:45 +0100

    final

--- js.js
+++ js.js
@@ -9,16 +9,33 @@ const modal = document.getElementById('simpleModal');
 const modalBtn = document.getElementById('modalBtn');
 const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
+/* burger close-open animation*/
 
+menuBtn.addEventListener('click', navToggle);
+function navToggle(e) {
+    if(!e.target.classList.contains('active')){
+    e.target.classList.add('active')
+    gsap.to('.line1', 0.5, {rotate: '45deg', y: 4})
+    gsap.to('.line2', 0, {display: 'none'})
+    gsap.to('.line3', 0.5, {rotate: '-45deg', y: -4})
+    
+    } else {
+    e.target.classList.remove('active')
+    gsap.to('.line1', 0.5, {rotate: '0', y: 0})
+    gsap.to('.line2', 0, {display: 'block'})
+    gsap.to('.line3', 0.5, {rotate: '-0', y: 0})
+    }
+}
+menuBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('showT')
+ })
+/* side nav arrow */
  btn.addEventListener('click', ()=> {
      cont.classList.toggle('show')
      arrow.classList.toggle('rotate')
  })
 
- menuBtn.addEventListener('click', ()=> {
-    menu.classList.toggle('showT')
- })
-
+ /* modal open/exit */
  modalBtn.addEventListener('click', ()=> {
     modal.style.display = 'block';
  });
@@ -27,7 +44,7 @@ const closeBtn = document.getElementsByClassName('closeBtn')[0];
     modal.style.display = 'none';
  });
  
-/* outside click */
+/* outside exit click */
 window.addEventListener('click', outsideClick);
 function outsideClick(e) {
     if(e.target == modal){
